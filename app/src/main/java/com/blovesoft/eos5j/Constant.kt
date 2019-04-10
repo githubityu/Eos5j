fccfe5b6186e492ebafc50c2efb7fa5171a3073a
yu.jl <yu.jl@bloveoa.com>
2019-04-10 14:51:22 +0800

    clear

--- app/src/main/java/com/blovesoft/eos5j/Constant.kt
+++ app/src/main/java/com/blovesoft/eos5j/Constant.kt
@@ -7,12 +7,12 @@ package com.blovesoft.eos5j
  *    desc  :
  */
 object Constant {
-    const val URL = "http://beitai.blove.io"
-    const val CREATE_ACCOUNT = "dyz121212121"
+    const val URL = ""
+    const val CREATE_ACCOUNT = ""
     const val CREATE_SYS = "eosio.token"
     const val CREATE_EOSIO = "eosio"
     const val ACTION_TRANSFER = "transfer"
-    const val PRIKEY = "5JLZRnbYVycrAWtibPkU53Ripvmz6Qykfc66g7H3N8z9XitM7sS"
+    const val PRIKEY = ""
     const val SYS_NAME = " EOS"
-    const val EOS_ACCOUNT = "EOS6TdhQBHLwrAzgu4wuBhBJD891v3JTC4JbZUaKV88eonCdsBqSB"
+    const val EOS_ACCOUNT = ""
 }
